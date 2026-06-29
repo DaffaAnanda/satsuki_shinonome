abee96d1fd5405b9609babc2c41148c6fb7d31f2
DaffaAnanda <muhammaddaffaananda2811@gmail.com>
2026-06-29 15:08:38 +0700

    Remove tab functionality and log message
    Removed tab functionality code and added a console log statement.

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const tabs = document.querySelectorAll(".tab");
-const posts = document.querySelectorAll(".post");
-
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    const filter = tab.dataset.filter;
-
-    tabs.forEach((item) => item.classList.remove("active"));
-    tab.classList.add("active");
-
-    posts.forEach((post) => {
-      const shouldShow = filter === "all" || post.dataset.type === filter;
-      post.classList.toggle("is-hidden", !shouldShow);
-    });
-  });
-});
+console.log("Portfolio website loaded.");
